4c122b050d7cd060b872aba020ba8b3706023034
Massimo Bonanni <massimo.bonanni@tiscali.it>
2024-11-11 18:53:33 +0100

    Add gitignore

--- FACEIT.Core/obj/Debug/net8.0/FACEIT.Core.AssemblyInfo.cs
+++ FACEIT.Core/obj/Debug/net8.0/FACEIT.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FACEIT.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf043efce50c97eb8116593667fe159641331006")]
 [assembly: System.Reflection.AssemblyProductAttribute("FACEIT.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FACEIT.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
