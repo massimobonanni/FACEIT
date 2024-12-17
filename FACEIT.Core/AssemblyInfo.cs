aecc631e9b13ab6ca76983995072924d0ebeff4e
Massimo Bonanni <massimo.bonanni@tiscali.it>
2024-12-17 10:35:40 +0100

    Add custom assembly info

---
+++ FACEIT.Core/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("Massimo Bonanni")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyProduct("FACEIT")]
+[assembly: AssemblyTitle("FACEIT.Core")]
+[assembly: AssemblyVersion("1.0.0.0")]
